6d4fe38914ac7804d1c37fff754da317a071ddec
dominiccodes <email@domcodes.com>
2017-07-19 20:10:44 +0100

    Added a user id to tasks, configured user task associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	# dependent: :destroy will delete all tasks of a deleted user so no orphan tasks
+	has_many :tasks, dependent: :destroy
 end

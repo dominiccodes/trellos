926735d86723af82870aa1b3ee7f7dbc402b5f24
dominiccodes <email@domcodes.com>
2017-07-20 19:35:51 +0100

    Added validations to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
 	belongs_to :user
 	
+	validates :user_id, presence: true
+	validates :content, presence: true
+	
   FORMAT = AutoHtml::Pipeline.new(
     AutoHtml::HtmlEscape.new,
     AutoHtml::Markdown.new,
